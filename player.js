c9770a9db708c14794a12eea1053c65d9ca6da21
boynedmaster <boynedmaster@gmail.com>
2016-04-17 15:33:30 -0700

    Created first version of player.js

---
+++ player.js
@@ -0,0 +1,16 @@
+class Player {
+  constructor(game, client, role){
+    this.game = game;
+    this.client = client;
+    this.role = role;
+  }
+
+  kill(reason){
+    let die = true;
+
+    if(this.role.can_kill(reason))
+      this.role.on_kill(reason);
+  }
+}
+
+module.exports = Player;
