a891f888fea09af3ae00519fbe01a90176b73350
boynedmaster <boynedmaster@gmail.com>
2016-04-19 06:26:40 -0700

    Added lynch meeting

---
+++ meetings/lynch_meeting.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const Meeting = require("./meeting.js").Meeting;
+const _ = require("underscore");
+
+class LynchMeeting extends Meeting {
+  constructor(game){
+    super(game);
+    this.id = "lynch";
+    this.name = "Village Meeting";
+    this.description = "Choose someone to lynch";
+    this.channel = "mafia";
+  }
+
+  can_vote_for(player){
+    return true;
+  }
+
+  show(game){
+    return game.day;
+  }
+
+  end(){
+    let victims = require("./meeting.js").majority(this.votes);
+
+    if(victims.length === 1){
+      let victim = victims[0];
+
+      victim.kill("lynch");
+    }
+  }
+}
+
+module.exports = LynchMeeting;
