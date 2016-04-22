4cb4b4dbf5d9ecce998d8dabb4e933b43c47e167
boynedmaster <boynedmaster@gmail.com>
2016-04-22 16:01:47 -0700

    Fixed lack of semicolons

--- roles/mafia.js
+++ roles/mafia.js
@@ -7,7 +7,7 @@ class Mafia extends Role {
     super(game, player);
     this.id = "mafia";
     this.name = "Mafia";
-    this.description = "Chooses one person to kill every night\nWins when the mafia outnumber the village"
+    this.description = "Chooses one person to kill every night\nWins when the mafia outnumber the village";
     this.alignment = "mafia";
     this.meetings.push("mafia");
   }
--- roles/role.js
+++ roles/role.js
@@ -22,7 +22,7 @@ class Role {
         ret = false;
         return;
       }
-    })
+    });
 
     return ret;
   }
